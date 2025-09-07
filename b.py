efdf2066f03b2a215a05e00b75e5d426ac1e0d62
nilay1516412824 <nilay.1516412824@std.ggsipu.ac.in>
2025-09-07 11:47:52 +0530

    Add files via upload

---
+++ b.py
@@ -0,0 +1,8 @@
+def my_function():
+    message = "Hello, I am a local variable!"  # Local variable
+    print(message)
+
+my_function()
+# print(message)  # This will cause an error because 'message' is local to my_function.
+
+    
